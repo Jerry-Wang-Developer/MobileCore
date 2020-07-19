262dba09f4a737d8839731cfa9d94817f353034f
jerrywang <445242970@qq.com>
2020-07-19 17:28:04 +0800

    Initial Commit

---
+++ MobileCore/MobileCore.h
@@ -0,0 +1,19 @@
+//
+//  MobileCore.h
+//  MobileCore
+//
+//  Created by 王小涛 on 2020/7/19.
+//  Copyright © 2020 王小涛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileCore.
+FOUNDATION_EXPORT double MobileCoreVersionNumber;
+
+//! Project version string for MobileCore.
+FOUNDATION_EXPORT const unsigned char MobileCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileCore/PublicHeader.h>
+
+
